c7b79b5a070ca648d63c3ce59010b1cdc2ed91f1
joshuatitens <titensj23@greenhill.org>
2020-06-21 23:58:48 -0500

    hi

--- src/com/company/StringChallenge.java
+++ src/com/company/StringChallenge.java
@@ -3,25 +3,32 @@ package com.company;
 //import Scanner
 // print line that declares amount of strings to be used in code
 //split the words up by every other letter
+//repeat for every word
 import java.util.Scanner;
 
 public class StringChallenge {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Please enter a word: ");
-        String phrase = input.nextLine();
-        String evens = "";
-        String odds = "";
+        for (int i = 0; i < 10; i = i + 1) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Please enter a word: ");
+            String phrase = input.nextLine();
+            String evens = "";
+            String odds = "";
 
-        for (int i = 0; i < phrase.length(); i+=2) {
+            for (int n = 0; i < phrase.length(); i += 2) {
                 evens = evens + phrase.charAt(i);
             }
-        for (int i = 1; i < phrase.length(); i+=2) {
-            odds = odds + phrase.charAt(i);
+            for (int n = 1; i < phrase.length(); i += 2) {
+                odds = odds + phrase.charAt(i);
             }
-                String answer = evens +" " +odds;
-        System.out.print(answer);
+            String answer = evens + " " + odds;
+            System.out.print(answer);
+
+        }
+
     }
 }
+
+
 
 
